02735c5615898d720eb1eca3228317e7b4904ca1
rivafarabi <rivafarabi.babeheer@gmail.com>
2019-07-06 22:17:04 +0700

    feat: deckboard-kit api implementation

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
-const {
-  DeckboardExtension,
-  extensionLog,
-  INPUT_METHOD
-} = require("deckboard-extension-kit");
-const { shutdown } = require("wintools");
+const { Extension, INPUT_METHOD, PLATFORMS } = require('deckboard-kit');
+const { shutdown } = require('wintools');
 
-const inputSelections = [
-  {
-    label: "Power Option",
-    value: "power-option",
-    icon: "power-off",
-    color: "#34495e",
-    input: [
-      {
-        label: "Action",
-        ref: "powerAction",
-        type: INPUT_METHOD.INPUT_SELECT,
-        items: [
-          {
-            label: "Shutdown",
-            value: "shutdown"
-          },
-          {
-            label: "Restart",
-            value: "restart"
-          }
-        ]
-      }
-    ]
-  }
-];
+class PowerControlExtension extends Extension {
+	constructor() {
+		super();
+		this.name = 'TEST';
+		this.platforms = [PLATFORMS.WINDOWS, PLATFORMS.MAC];
+		this.inputs = [
+			{
+				label: 'Power Option',
+				value: 'power-option',
+				icon: 'power-off',
+				color: '#34495e',
+				input: [
+					{
+						label: 'Action',
+						ref: 'powerAction',
+						type: INPUT_METHOD.INPUT_SELECT,
+						items: [
+							{
+								label: 'Shutdown',
+								value: 'shutdown'
+							},
+							{
+								label: 'Restart',
+								value: 'restart'
+							}
+						]
+					}
+				]
+			}
+		];
+	}
 
-const execute = (action, { powerAction }) => {
-  extensionLog("info", `${action} ${powerAction}`);
-  switch (action) {
-    case "power-option": {
-      switch (powerAction) {
-        case "shutdown":
-          shutdown.poweroff();
-          break;
-        case "restart":
-          shutdown.restart();
-          break;
-        default:
-          break;
-      }
-    }
-    default:
-      break;
-  }
-};
+	execute = (action, { powerAction }) => {
+		extensionLog('info', `${action} ${powerAction}`);
+		switch (action) {
+			case 'power-option': {
+				switch (powerAction) {
+					case 'shutdown':
+						shutdown.poweroff();
+						break;
+					case 'restart':
+						shutdown.restart();
+						break;
+					default:
+						break;
+				}
+			}
+			default:
+				break;
+		}
+	};
+}
 
-const extension = new DeckboardExtension(
-  "Power Options",
-  inputSelections,
-  execute
-);
-module.exports = extension;
+module.exports = new PowerControlExtension();
